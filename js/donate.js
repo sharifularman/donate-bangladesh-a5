b902576d8a0754de004ffdcca8b7456c4afaaf63
sharifularman <sharifulislamarman20@gmail.com>
2024-12-10 22:29:18 +0600

    done

--- js/donate.js
+++ js/donate.js
@@ -1,28 +1,71 @@
 // donate for Noakhali flood affected people btn
-document.getElementById("donate-noakhali-btn").addEventListener("click", function () {
+const donationForNoakhali = document.getElementById("donate-noakhali-btn");
+donationForNoakhali.addEventListener("click", function () {
     const startingDonationAmount = getTextElementByIdName("total-amount-noakhali");
-    const donationAmountInput = getInputValueByIdName("noakhali-donation-amount");
+    const inputAmount = getInputValueByIdName("noakhali-donation-amount");
     const accountBalance = getTextElementByIdName("account-balance");
     const historyContainer = document.getElementById("history-container");
 
-    if (donationAmountInput <= accountBalance) {
-        const totalDonationAmount = startingDonationAmount + donationAmountInput;
-        const remainingBalance = accountBalance - donationAmountInput;
+    if (!isNaN(inputAmount) && inputAmount <= accountBalance && inputAmount > 0) {
+        const totalDonationAmount = (startingDonationAmount + inputAmount).toFixed(2);
+        const remainingBalance = (accountBalance - inputAmount).toFixed(2);
         document.getElementById("total-amount-noakhali").innerText = totalDonationAmount;
         document.getElementById("account-balance").innerText = remainingBalance;
-        const div = document.createElement("div");
-        div.style.paddingBottom = "20px";
-        div.innerHTML = `
-     <h2 class="font-bold text-xl">${donationAmountInput} bdt added at 10.10 pm</h2>
-    <p>Thank you for the contribution</p>`
+        const div = divInnerHtml('noakhali-donation-amount')
         historyContainer.appendChild(div)
-        // alert('You have successfully donate')
+        alert('Congratulations! You have successfully donated')
     }
     else {
         alert("Please enter a valid amount")
     }
-    document.getElementById("noakhali-donation-amount").value = "";
-    document.getElementById("modal").classList.remove("hidden");
+    clearInputField('noakhali-donation-amount').value = "";
+    // document.getElementById("modal").classList.remove("hidden");
 });
 
+// donate for feni flood affected people 
+const donationForFeni = document.getElementById('feni');
+donationForFeni.addEventListener("click", function () {
+    const initialDonationAmount = getTextElementByIdName("feni-initial-amount");
+    const inputAmount = getInputValueByIdName("input-amount");
+    const accountBalance = getTextElementByIdName("account-balance");
+    if (!isNaN(inputAmount) && inputAmount <= accountBalance && inputAmount > 0) {
+        const totalDonationAmount =( initialDonationAmount + inputAmount).toFixed(2);
+        const feniDonationAmount = document.getElementById("feni-initial-amount");
+        const remainingBalance = (accountBalance - inputAmount).toFixed(2);
+        document.getElementById("account-balance").innerText = remainingBalance;
+        feniDonationAmount.innerText = totalDonationAmount;
 
+        const div = divInnerHtml('input-amount')
+        historyContainer.appendChild(div)
+        alert("Congratulations! You have successfully donated")
+    }
+    else {
+        alert("Please enter a valid amount");
+    }
+    clearInputField('input-amount').value = ""
+
+});
+
+// Aid for Injured in the Quota Movement
+const aidForQuotaMovement = document.getElementById("quota-movement");
+aidForQuotaMovement.addEventListener("click", function () {
+    const initialDonationAmount = getTextElementByIdName("aid-amount");
+    const inputAmount = getInputValueByIdName("input-aid-amount");
+    const accountBalance = getTextElementByIdName("account-balance");
+    if (!isNaN(inputAmount) && inputAmount <= accountBalance && inputAmount > 0) {
+        const totalDonationAmount = (initialDonationAmount + inputAmount).toFixed(2);
+        const aidDonationAmount = document.getElementById("aid-amount");
+        const remainingBalance = (accountBalance - inputAmount).toFixed(2);
+        document.getElementById("account-balance").innerText = remainingBalance;
+        aidDonationAmount.innerText = totalDonationAmount;
+
+        const div = divInnerHtml('input-aid-amount');
+        historyContainer.appendChild(div);
+        alert("Congratulations! You have successfully donated")
+    }
+    else {
+        alert("Please enter a valid amount");
+    }
+
+    clearInputField('input-aid-amount').value = "";
+});
--- js/utilitize.js
+++ js/utilitize.js
@@ -7,3 +7,24 @@ function getInputValueByIdName(id) {
     const donationAmount = parseFloat(document.getElementById(id).value);
     return donationAmount;
 }
+
+// clear the input field 
+function clearInputField(id) {
+    const inputField = document.getElementById(id);
+    return inputField
+}
+
+// create a div for the history section
+function divInnerHtml(id) {
+    const inputAmount = getInputValueByIdName(id);
+    const dateTime = new Date();
+    const div = document.createElement("div");
+        div.style.paddingBottom = "20px";
+        div.innerHTML = `
+        <div class="w-4/5 mx-auto border border-green-200 shadow-sm p-4 text-center">
+     <h2 class="font-semibold text-xl">${inputAmount} BDT added at ${dateTime}</h2>
+    <p>Thank you for the contribution</p>
+    </div>`
+    return div;
+}
+
